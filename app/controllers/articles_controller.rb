ba37eb54e81c8cdcfc55e4efde8c0cd443d0f2a6
Elinore Tenorio <elinore.tenorio@gmail.com>
2014-01-21 18:59:19 +0800

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'welcome',
+  password: 'secret',
+  except: [:index, :show]
+
+  def index
+  	@articles = Article.all
+  end
+
+  def new
+  	@article = Article.new
+  end
+
+  def create
+  	Article.create(params.require(:article).permit(:title, :description))
+  	redirect_to articles_path
+  end
+
+  def edit
+  	@article = Article.find(params[:id])
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+  	@article.update_attributes(params.require(:article).permit(:title, :description))
+  	
+  	redirect_to articles_path
+  end
+
+  def show
+  	@article = Article.find(params[:id])
+  end
+
+  def destroy
+  	@article = Article.find(params[:id])
+  	@article.destroy
+
+  	redirect_to articles_path, notice: 'Delete success'
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'b8559b89f12bc2197ea34ddbef1c56b891f21b131a1268aad9b9fef84b145b5822dd3e44b94d935d6c4715dc62b7caaeecccaee0392ee2ec90c1bc3912f55778'
